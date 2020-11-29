1a232b2c075c7cee03f0b75d2bf2077ee4b5a4d2
liugenghua <liugenghua@bmsoft.com.cn>
2020-11-29 18:32:11 +0800

    更新表设计

--- Week_06/shop.sql
+++ Week_06/shop.sql
@@ -1,162 +1,190 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 本地数据库
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 80016
- Source Host           : localhost:3306
+ Source Server Version : 50731
+ Source Host           : localhost:3307
  Source Schema         : shop
 
  Target Server Type    : MySQL
- Target Server Version : 80016
+ Target Server Version : 50731
  File Encoding         : 65001
 
- Date: 25/11/2020 19:44:39
+ Date: 29/11/2020 18:29:01
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for t_account
+-- Table structure for tb_account
 -- ----------------------------
-DROP TABLE IF EXISTS `t_account`;
-CREATE TABLE `t_account`  (
-  `id` bigint(20) NOT NULL COMMENT '账户id',
-  `user_id` bigint(20) NULL DEFAULT NULL COMMENT '用户id',
-  `account` decimal(10, 2) NULL DEFAULT NULL COMMENT '账户余额',
-  `create_time` bigint(20) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` bigint(20) NULL DEFAULT NULL COMMENT '更新时间',
+DROP TABLE IF EXISTS `tb_account`;
+CREATE TABLE `tb_account`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '账户id',
+  `user_id` bigint(20) NOT NULL COMMENT '用户id',
+  `account` decimal(10, 2) NOT NULL DEFAULT 0.00 COMMENT '账户余额',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `index_user_id`(`user_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  INDEX `idx_user_id`(`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for t_account_detail
+-- Table structure for tb_address
 -- ----------------------------
-DROP TABLE IF EXISTS `t_account_detail`;
-CREATE TABLE `t_account_detail`  (
-  `id` bigint(20) NOT NULL COMMENT '账户明细id',
-  `account_id` bigint(20) NULL DEFAULT NULL COMMENT '账户id',
-  `user_id` int(11) NULL DEFAULT NULL COMMENT '用户id',
-  `order_number` bigint(20) NULL DEFAULT NULL COMMENT '订单号',
-  `change_reason` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '0' COMMENT '变更原因（0：下订单，1：退货单）',
-  `change_time` bigint(20) NULL DEFAULT NULL COMMENT '变更时间',
-  `change_mount` decimal(10, 2) NULL DEFAULT NULL COMMENT '变更金额',
-  `create_time` bigint(20) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` bigint(20) NULL DEFAULT NULL COMMENT '修改时间',
+DROP TABLE IF EXISTS `tb_address`;
+CREATE TABLE `tb_address`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '地址id',
+  `user_id` bigint(20) NOT NULL COMMENT '用户id',
+  `address` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '地址',
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '姓名',
+  `phone` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '手机号码',
+  `postcode` char(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '邮编',
+  `state` tinyint(4) NOT NULL DEFAULT 0 COMMENT '是否默认',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `index_account_id`(`account_id`) USING BTREE,
-  INDEX `index_user_id`(`user_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  INDEX `idx_user_id`(`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for t_address
+-- Table structure for tb_category
 -- ----------------------------
-DROP TABLE IF EXISTS `t_address`;
-CREATE TABLE `t_address`  (
-  `id` bigint(20) NOT NULL COMMENT '地址id',
-  `user_id` bigint(20) NULL DEFAULT NULL COMMENT '用户id',
-  `address` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '地址',
-  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '姓名',
-  `phone` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号码',
-  `postcode` char(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '邮编',
-  `state` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '是否默认',
-  `create_time` bigint(20) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` bigint(20) NULL DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `index_user_id`(`user_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for t_category
--- ----------------------------
-DROP TABLE IF EXISTS `t_category`;
-CREATE TABLE `t_category`  (
-  `id` bigint(20) NOT NULL COMMENT '分类id',
-  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '分类名称',
-  `code` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '分类编码',
-  `state` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '状态',
-  `create_time` bigint(20) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` bigint(20) NULL DEFAULT NULL COMMENT '更新时间',
+DROP TABLE IF EXISTS `tb_category`;
+CREATE TABLE `tb_category`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '分类id',
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '分类名称',
+  `code` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '分类编码',
+  `state` tinyint(4) NOT NULL DEFAULT 0 COMMENT '状态',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for t_goods
+-- Table structure for tb_goods
 -- ----------------------------
-DROP TABLE IF EXISTS `t_goods`;
-CREATE TABLE `t_goods`  (
-  `id` bigint(20) NOT NULL COMMENT '商品id',
-  `category_id` bigint(20) NULL DEFAULT NULL COMMENT '分类id',
-  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '商品名称',
-  `description` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '商品描述',
-  `image` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '商品图片',
-  `price` decimal(10, 2) NULL DEFAULT NULL COMMENT '商品单价',
-  `production_date` bigint(20) NULL DEFAULT NULL COMMENT '生产日期',
-  `valid_date` bigint(20) NULL DEFAULT NULL COMMENT '商品有效期',
-  `state` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '状态',
+DROP TABLE IF EXISTS `tb_goods`;
+CREATE TABLE `tb_goods`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品id',
+  `category_id` bigint(20) NOT NULL COMMENT '分类id',
+  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品名称',
+  `description` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品描述',
+  `image` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品图片',
+  `production_date` date NOT NULL COMMENT '生产日期',
+  `valid_date` bigint(20) NOT NULL COMMENT '商品有效期',
+  `state` tinyint(1) NOT NULL DEFAULT 0 COMMENT '状态',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `index_category_id`(`category_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  INDEX `idx_category_id`(`category_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for t_order
+-- Table structure for tb_goods_sku
 -- ----------------------------
-DROP TABLE IF EXISTS `t_order`;
-CREATE TABLE `t_order`  (
-  `id` bigint(20) NOT NULL COMMENT '订单id',
-  `user_id` bigint(20) NULL DEFAULT NULL COMMENT '用户id',
-  `user_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户名',
-  `order_number` bigint(20) NULL DEFAULT NULL COMMENT '订单号',
-  `receiver` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '收货人',
-  `receiver_phone` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '收货人手机号码',
-  `receiver_address` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '收货地址',
-  `order_time` bigint(20) NULL DEFAULT NULL COMMENT '下单时间',
-  `express_number` bigint(20) NULL DEFAULT NULL COMMENT '快递单号',
-  `express_company` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '物流公司',
-  `order_mount` decimal(10, 2) NULL DEFAULT NULL COMMENT '订单金额',
-  `state` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '状态',
-  `create_time` bigint(20) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` bigint(20) NULL DEFAULT NULL COMMENT '修改时间',
+DROP TABLE IF EXISTS `tb_goods_sku`;
+CREATE TABLE `tb_goods_sku`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'skuid',
+  `goods_id` bigint(20) NOT NULL COMMENT '商品id',
+  `sku` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'sku属性',
+  `price` decimal(10, 2) NOT NULL DEFAULT 0.00 COMMENT '价格',
+  `stock` int(11) NOT NULL DEFAULT 0 COMMENT '库存',
+  `address` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '地址',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `index_user_id`(`user_id`) USING BTREE,
-  INDEX `index_express_number`(`express_number`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  INDEX `idx_goods_id`(`goods_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for t_order_snapshot
+-- Table structure for tb_order
 -- ----------------------------
-DROP TABLE IF EXISTS `t_order_snapshot`;
-CREATE TABLE `t_order_snapshot`  (
-  `id` bigint(20) NOT NULL COMMENT '快照id',
-  `order_id` bigint(20) NULL DEFAULT NULL COMMENT '订单id',
-  `goods_id` bigint(20) NULL DEFAULT NULL COMMENT '商品id',
-  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '商品名称',
-  `amount` int(11) NULL DEFAULT NULL COMMENT '商品数量',
-  `price` decimal(10, 2) NULL DEFAULT NULL COMMENT '商品单价',
-  `total_price` decimal(10, 2) NULL DEFAULT NULL COMMENT '商品总价',
-  `create_time` bigint(20) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` bigint(20) NULL DEFAULT NULL COMMENT '修改时间',
+DROP TABLE IF EXISTS `tb_order`;
+CREATE TABLE `tb_order`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '订单id',
+  `user_id` bigint(20) NOT NULL COMMENT '用户id',
+  `user_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
+  `order_number` bigint(20) NOT NULL COMMENT '订单号',
+  `receiver` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '收货人',
+  `receiver_phone` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '收货人手机号码',
+  `receiver_address` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '收货地址',
+  `order_time` bigint(20) NOT NULL COMMENT '下单时间',
+  `express_number` bigint(20) NOT NULL COMMENT '快递单号',
+  `express_company` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '物流公司',
+  `order_mount` decimal(10, 2) NOT NULL COMMENT '订单金额',
+  `state` tinyint(4) NOT NULL DEFAULT 0 COMMENT '状态',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `index_order_id`(`order_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  INDEX `idx_user_id`(`user_id`) USING BTREE,
+  INDEX `idx_express_number`(`express_number`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for t_user
+-- Table structure for tb_order_snapshot
 -- ----------------------------
-DROP TABLE IF EXISTS `t_user`;
-CREATE TABLE `t_user`  (
-  `id` bigint(20) NOT NULL COMMENT '用户id',
-  `user_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户名',
-  `login_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '账号',
-  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码',
-  `phone` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号码',
-  `gender` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '性别',
-  `state` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户状态',
-  `create_time` bigint(20) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` bigint(20) NULL DEFAULT NULL COMMENT '修改时间',
+DROP TABLE IF EXISTS `tb_order_snapshot`;
+CREATE TABLE `tb_order_snapshot`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '快照id',
+  `order_id` bigint(20) NOT NULL COMMENT '订单id',
+  `goods_id` bigint(20) NOT NULL COMMENT '商品id',
+  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品名称',
+  `amount` int(11) NOT NULL COMMENT '商品数量',
+  `price` decimal(10, 2) NOT NULL COMMENT '商品单价',
+  `total_price` decimal(10, 2) NOT NULL COMMENT '商品总价',
+  `sku` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品sku',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_order_id`(`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for tb_sku_key
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_sku_key`;
+CREATE TABLE `tb_sku_key`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '属性key_id',
+  `goods_id` bigint(20) NOT NULL COMMENT '商品id',
+  `key_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '属性名',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_goods_id`(`goods_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for tb_sku_value
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_sku_value`;
+CREATE TABLE `tb_sku_value`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '属性value_id',
+  `key_id` bigint(20) NOT NULL COMMENT '属性key_id',
+  `value` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '属性值',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_key_id`(`key_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for tb_user
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE `tb_user`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `user_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
+  `login_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '账号',
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
+  `phone` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '手机号码',
+  `gender` tinyint(4) NOT NULL DEFAULT 1 COMMENT '性别',
+  `state` tinyint(4) NOT NULL DEFAULT 0 COMMENT '用户状态',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
